ae93fa994a5d06e06bb3eeea2de8987267a80323
Vincent Handoyo <vincenthan28@gmail.com>
2025-08-14 23:53:32 +0800

    delete: remove public/demo

--- public/demo/incl/settings.php
+++
@@ -1,6 +0,0 @@
-<!--CSS-->
-<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900' rel='stylesheet' type='text/css'>
-<link href='https://fonts.googleapis.com/css?family=Comfortaa:400,700,300' rel='stylesheet' type='text/css'>
-<link rel="stylesheet" href="css/iconfonts.min.css" />
-<link rel="stylesheet" href="css/flex.min.css" />
-<link rel="stylesheet" href="css/magz.css" />
